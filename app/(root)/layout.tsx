26dcde5eeecd8aeeda86fd2a35bd7326d8daaad3
fahad39 <fahadhussain0127@gmail.com>
2024-01-03 19:07:16 +0500

    added button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
+import NavBar from "@/components/navbar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <NavBar></NavBar>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
