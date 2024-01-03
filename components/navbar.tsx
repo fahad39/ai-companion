ef1e8b9559e82090e650aacdc54b57309b5e1287
fahad39 <fahadhussain0127@gmail.com>
2024-01-03 19:33:49 +0500

    added theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import { Menu, Sparkle } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
 
 const font = Poppins({
   weight: "600",
@@ -33,6 +34,7 @@ const NavBar = () => {
           Upgrade
           <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
         </Button>
+        <ModeToggle />
         <UserButton />
       </div>
     </div>
