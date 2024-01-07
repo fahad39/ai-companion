499978ca7e59270de50f843de19e59a59274586d
fahad39 <fahadhussain0127@gmail.com>
2024-01-07 15:25:13 +0500

    cloudinary connection

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -4,6 +4,14 @@ import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
 
 interface CompanionForm {
   initalData: Companion | null;
@@ -50,7 +58,32 @@ const CompanionForm = ({ initalData, categories }: CompanionForm) => {
     console.log(values);
   };
 
-  return <div>CompanionForm</div>;
+  return (
+    <div className="h-full space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="space-y-2 w-full ">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General Information about your companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4">
+                <FormControl>Image Upload Component</FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CompanionForm;
