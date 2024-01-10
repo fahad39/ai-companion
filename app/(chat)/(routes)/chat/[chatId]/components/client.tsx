e46ea997efb2f4af50fcd116e04adc85a8f5e4a6
fahad39 <fahadhussain0127@gmail.com>
2024-01-10 13:34:50 +0500

    added chat messages

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 import { useCompletion } from "ai/react";
 import ChatForm from "@/components/chat-form";
+import ChatMessages from "@/components/chat-messages";
 
 interface ChatClientProps {
   companion: Companion & {
@@ -45,7 +46,11 @@ const ChatClient = ({ companion }: ChatClientProps) => {
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
       <ChatHeader companion={companion} />
-      <div>Messages TODO</div>
+      <ChatMessages
+        companion={companion}
+        isLoading={isLoading}
+        messages={messages}
+      />
       <ChatForm
         isLoading={isLoading}
         input={input}
