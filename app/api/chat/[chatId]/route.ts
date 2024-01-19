dadc3151de49efbc69c1bd2d810a482adcd0ddae
fahad39 <fahadhussain0127@gmail.com>
2024-01-19 23:28:08 +0500

    added code

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,72 @@
+import { StreamingTextResponse, LangChainStream } from "ai";
+import { auth, currentUser } from "@clerk/nextjs";
+import { CallbackManager } from "langchain/callbacks";
+import { Replicate } from "langchain/llms/replicate";
+import { NextResponse } from "next/server";
+
+import { MemoryManager } from "@/lib/memory";
+import { rateLimit } from "@/lib/rate-limit";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { chatId: string } }
+) {
+  try {
+    const { prompt } = await request.json();
+    const user = await currentUser();
+
+    if (!user || !user.firstName || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const identifier = request.url + "-" + user.id;
+    const { success } = await rateLimit(identifier);
+
+    if (!success) {
+      return new NextResponse("Rate Limit Exceed", { status: 429 });
+    }
+
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.chatId,
+        userId: user.id,
+      },
+      data: {
+        messages: {
+          create: {
+            content: prompt,
+            role: "user",
+            userId: user.id,
+          },
+        },
+      },
+    });
+
+    if (!companion) {
+      return new NextResponse("Companion not found", { status: 404 });
+    }
+
+    const name = companion.id;
+    const companion_file_name = name + ".txt";
+
+    const companionKey = {
+      comapanionName: name,
+      userId: user.id,
+      modelName: "llama2-13b",
+    };
+
+    const memoryManager = await MemoryManager.getInstance();
+
+    const records = await memoryManager.readLatestHistory(companionKey);
+
+    if (records.length === 0) {
+      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
+    }
+
+    await memoryManager.writeToHistory("user: " + prompt + "\n", companionKey);
+  } catch (error) {
+    console.log("[CHAT_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
