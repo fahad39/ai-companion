c08ee337918a90e53fcbc6b0629b6ea62eb34865
fahad39 <fahadhussain0127@gmail.com>
2024-01-22 22:55:42 +0500

    added code

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -31,7 +31,6 @@ export async function POST(
     const companion = await prismadb.companion.update({
       where: {
         id: params.chatId,
-        userId: user.id,
       },
       data: {
         messages: {
@@ -110,6 +109,36 @@ export async function POST(
         )
         .catch(console.error)
     );
+
+    const cleaned = resp.replaceAll(",", "");
+    const chunks = cleaned.split("\n");
+    const response = chunks[0];
+
+    await memoryManager.writeToHistory("" + response.trim(), companionKey);
+    var Readable = require("stream").Readable;
+    let s = new Readable();
+    s.push(response);
+    s.push(null);
+
+    if (response !== undefined && response.length > 1) {
+      memoryManager.writeToHistory("" + response.trim(), companionKey);
+      await prismadb.companion.update({
+        where: {
+          id: params.chatId,
+        },
+        data: {
+          messages: {
+            create: {
+              content: response.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        },
+      });
+    }
+
+    return new StreamingTextResponse(s);
   } catch (error) {
     console.log("[CHAT_POST]", error);
     return new NextResponse("Internal Error", { status: 500 });
